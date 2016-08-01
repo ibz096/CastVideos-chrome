4e572e18e6b0db0121a931d1695120b42ef7e308
Ibrahim Ahmed <ibrahim.ahmed1996@gmail.com>
2016-08-01 16:06:10 -0400

    Update CastVideos.js

--- CastVideos.js
+++ CastVideos.js
@@ -352,7 +352,7 @@ CastPlayer.prototype.loadMedia = function(mediaIndex) {
     return;
   }
   console.log("loading..." + this.mediaContents[mediaIndex]['title']);
-  var mediaInfo = new chrome.cast.media.MediaInfo('http://dreamsupport.us/justin/TV%20Shows/Breaking%20Bad/Season%202/Breaking%20Bad%20Season%202%20Episode%2002%20-%20Grilled.avi'/*this.mediaContents[mediaIndex]['sources'][0]*/);
+  var mediaInfo = new chrome.cast.media.MediaInfo(this.mediaContents[mediaIndex]['sources'][0]);
 
   mediaInfo.metadata = new chrome.cast.media.GenericMediaMetadata();
   mediaInfo.metadata.metadataType = chrome.cast.media.MetadataType.GENERIC;
@@ -1123,7 +1123,7 @@ CastPlayer.prototype.addVideoThumbs = function() {
 var mediaJSON = { "categories" : [ { "name" : "Movies",
         "videos" : [ 
 		    { "description" : "Big Buck Bunny tells the story of a giant rabbit with a heart bigger than himself. When one sunny day three rodents rudely harass him, something snaps... and the rabbit ain't no bunny anymore! In the typical cartoon tradition he prepares the nasty rodents a comical revenge.\n\nLicensed under the Creative Commons Attribution license\nhttp://www.bigbuckbunny.org",
-              "sources" : [ "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" ],
+              "sources" : [ "http://dreamsupport.us/justin/TV%20Shows/Breaking%20Bad/Season%202/Breaking%20Bad%20Season%202%20Episode%2002%20-%20Grilled.avi" ],
               "subtitle" : "By Blender Foundation",
               "thumb" : "images/BigBuckBunny.jpg",
               "title" : "Big Buck Bunny"
