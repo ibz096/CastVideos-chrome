c1aa6c3889786696a9afdee01389474c0aa2b6ab
Ibrahim Ahmed <ibrahim.ahmed1996@gmail.com>
2016-08-21 11:10:19 -0400

    Update CastVideos.js

--- CastVideos.js
+++ CastVideos.js
@@ -160,7 +160,6 @@ CastPlayer.prototype.initializeCastPlayer = function() {
 
   chrome.cast.initialize(apiConfig, this.onInitSuccess.bind(this), this.onError.bind(this));
 
-  this.addVideoThumbs();
   this.initializeUI();
 };
 
